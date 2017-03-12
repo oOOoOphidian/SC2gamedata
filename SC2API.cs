00d691baad1e694042a5af9c6531e516a2137c33
oOOoOphidian <ryan.cimera@gmail.com>
2017-03-12 14:21:49 -0600

    Add files via upload

---
+++ SC2API.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace SimpleSC2data
+{
+    public class SC2API
+    {
+        public HttpResponseMessage runRequest(string request)
+        {
+            HttpClient client = new HttpClient();
+            return client.GetAsync("http://localhost:6119/" + request).Result;
+        }
+
+        public class uiReq
+        {
+            public List<string> activeScreens { get; set; }
+        }
+        public class gameReq
+        {
+            public class Player
+            {
+                public int id { get; set; }
+                public string name { get; set; }
+                public string type { get; set; }
+                public string race { get; set; }
+                public string result { get; set; }
+            }
+            public bool isReplay { get; set; }
+            public double displayTime { get; set; }
+            public List<Player> players { get; set; }
+        }
+    }
+}
---
+++ SimpleSC2data.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Timers;
+using System.IO;
+
+namespace SimpleSC2data
+{
+    public partial class simplesc2data : Form
+    {
+        public simplesc2data()
+        {
+            InitializeComponent();
+            System.Timers.Timer updateTimer = new System.Timers.Timer();
+            updateTimer.Elapsed += new System.Timers.ElapsedEventHandler(updateData);
+            updateTimer.Interval = 1000;
+            updateTimer.Enabled = true;
+            l_filepath.Text = Environment.CurrentDirectory + "\\Players.txt";
+        }
+
+        private void updateData(object sender, ElapsedEventArgs e)
+        {
+            SC2API sc2API = new SC2API();
+
+            //SC2 Game State
+            HttpResponseMessage sc2response = sc2API.runRequest("game");
+            if (sc2response.IsSuccessStatusCode)
+            {
+                string sc2result = sc2response.Content.ReadAsStringAsync().Result;
+                SC2API.gameReq gameResult = JsonConvert.DeserializeObject<SC2API.gameReq>(sc2result);
+                if (gameResult.players.Count == 2)
+                {
+                    File.WriteAllText(l_filepath.Text, "(" + gameResult.players[0].race[0] + ")" + gameResult.players[0].name + " vs. (" + gameResult.players[1].race[0] + ")" + gameResult.players[1].name);
+                }
+                else if (gameResult.players.Count == 4)
+                {
+                    File.WriteAllText(l_filepath.Text, gameResult.players[0].name + "/" + gameResult.players[1].name + " vs. " + gameResult.players[2].name + "/" + gameResult.players[3].name);
+                }
+            }
+        }
+    }
+}
